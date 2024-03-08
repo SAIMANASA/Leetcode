c0663dce2233273c864530898d5a2d0c8e0d80e1
SAIMANASA <saimanasa.kanduri@yahoo.com>
2024-03-08 15:01:48 -0800

    Time: 124 ms (56.40%), Space: 18 MB (29.30%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        #frontsum=[nums[0]]
+        frontsum=[nums[0]]
+        for i in range(1,len(nums)):
+            frontsum.append(nums[i]+frontsum[i-1])
+        #backsum
+        backsum=[nums[len(nums)-1]]
+        for i in range(len(nums)-2,-1,-1):
+            backsum.append(nums[i]+backsum[(len(nums)-1)-i-1])
+        backsum.reverse()
+        
+        for i in range(0,len(nums)):
+            if frontsum[i]==backsum[i]:
+                return i
+        return -1
+
+
+
+        

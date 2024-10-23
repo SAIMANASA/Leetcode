fde704751908f8d04d9e45ae6affa6ff17280f06
SAIMANASA <saimanasa.kanduri@yahoo.com>
2024-10-23 11:26:03 -0700

    Time: 0 ms (100%), Space: 16.8 MB (26.72%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -3,7 +3,7 @@ from collections import Counter
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         new=Counter(arr)
-        print(new)
+        
         n1=set()
         for i in new.values():
             if i in n1:
@@ -11,6 +11,15 @@ class Solution:
             else:
                 n1.add(i)
         return True
+    # return True if len(set(counts.values())) == len(counts.values()) else False if used sets
+
+    """
+    freq = {}
+        for x in arr:
+            freq[x] = freq.get(x, 0) + 1
+
+        return len(freq) == len(set(freq.values()))
+        """
                 
             
 

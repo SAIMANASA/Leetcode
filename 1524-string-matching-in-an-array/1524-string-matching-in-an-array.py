d2ae70574c5e7a90414ca85746a097948417062f
SAIMANASA <saimanasa.kanduri@yahoo.com>
2025-01-07 15:00:32 -0800

    Update 1524-string-matching-in-an-array.py

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -56,4 +56,14 @@ class Solution:
                 else:
                     main_index += 1
         return False  # No match found.
-        
+
+"""""
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans = set()
+        for x in words:
+            for y in words:
+                if x != y and x in y:
+                    ans.add(x)
+        return list(ans)
+""""

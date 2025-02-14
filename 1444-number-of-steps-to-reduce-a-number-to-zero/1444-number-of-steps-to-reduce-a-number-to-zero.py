316a2b50d0eaadf034120ffc01ef1017dc64dae4
SAIMANASA <saimanasa.kanduri@yahoo.com>
2025-02-14 09:54:57 -0800

    Time: 0 ms (100%), Space: 17.9 MB (17.32%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,5 +1,6 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
+        #https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/solutions/6397373/two-solutions-looping-integer-operations-bitwise-operation
         """
          count=0
         while(num!=0):
@@ -33,6 +34,10 @@ class Solution:
         return 1 + self.numberOfSteps(num - 1 if num & 1 else num >> 1)
     """
     """
+    The bit_length() function in Python is used to determine the number of bits necessary to represent an integer in binary, excluding the sign and leading zeros. It effectively calculates the length of the binary representation of an integer.
+
+There is also the Python Bin function. The bin() function in Python converts an integer to its binary string representation
+
     def numberOfSteps(self, num: int) -> int:
         if num == 0:
             return 0

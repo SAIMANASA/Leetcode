a4688dea0314728ba464050d1009c3546bfbf455
SAIMANASA <saimanasa.kanduri@yahoo.com>
2025-02-25 14:00:08 -0800

    Time: 40 ms (15.17%), Space: 23.5 MB (40.84%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,5 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
         ans=[0 for i in nums]
         ###prefix and postfix multiplication , doesn't need 2 arrays for prefix and postfix
         prefix=1
@@ -9,11 +10,10 @@ class Solution:
         for i in range(0,len(nums)-1):
             ans[i+1]=prefix*nums[i]
             prefix=prefix*nums[i]
+        print(ans)
         ##postfix multiplication into ans
         for i in range(len(nums)-2,-1,-1):
             postfix=postfix*nums[i+1]
             ans[i]=postfix*ans[i]
         return ans
         
-        
-       

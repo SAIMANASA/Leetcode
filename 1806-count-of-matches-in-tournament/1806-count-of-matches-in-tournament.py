35449f29dc6b86da493c4e29b27a9a0b792d6e36
SAIMANASA <saimanasa.kanduri@yahoo.com>
2025-02-27 10:37:13 -0800

    Time: 28 ms (97.12%), Space: 17.8 MB (29.95%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.py
@@ -0,0 +1,6 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        #https://leetcode.com/problems/count-of-matches-in-tournament/solutions/4364169/faster-lesser-3-methods-simple-math-iterative-solution-recursive-solution-visualized-too
+        #check neetcode
+        return n-1
+        
